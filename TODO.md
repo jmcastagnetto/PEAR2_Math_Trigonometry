23e8a68efd75293f57fbe36e22247928cbe1018f
Jesus M. Castagnetto <jesus@upch.pe>
2011-03-22 15:25:12 -0500

    Initial import of the port from Math_TriOp

---
+++ TODO.md
@@ -0,0 +1,3 @@
+* Create a valid package.xml file so Pyrus is happy
+* Create valid unit tests
+* Create user documentation
---
+++ example.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'Math/Trigonometry.php';
+use Math\Trigonometry; 
+
+echo Trigonometry::sin(1.5)."\n";
+echo Trigonometry::sinh(1.5)."\n";
+?>
